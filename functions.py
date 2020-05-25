7d6a468c4d53f6bdcb7e43288c96b937f260b483
Giovani Meneguel <34317514+GioAM@users.noreply.github.com>
2020-05-25 14:56:08 -0300

    busca heuristica

--- functions.py
+++ functions.py
@@ -3,42 +3,14 @@ from models import *
 from random import *
 
 
-def popular_mapa(mapa, x, y):
-    mapa_a_popular = []
-    for i in range(x):
-        mapa_a_popular.append([])
-        for j in range(y):
-            if mapa[i][j] == "G":
-                coordenada = Coordenada('G', i, j, 10)
-            elif mapa[i][j] == "A":
-                coordenada = Coordenada('A', i, j, 1)
-            elif mapa[i][j] == "P":
-                coordenada = Coordenada('P', i, j, 3)
-            elif mapa[i][j] == "T":
-                coordenada = Coordenada('T', i, j, 6)
-            elif mapa[i][j] == "E":
-                coordenada = Coordenada('E', i, j, 0)
-            mapa_a_popular[i].append(coordenada)
-
-    return mapa_a_popular
-
-
-def mostrar_mapa(mapa, x, y):
-    for i in range(x):
-        for j in range(y):
-            print(mapa[i][j], end=" ")
-        print('')
-    print("")
-
-
 def sortear_revendas(mapa):
     revendas = []
     numero_de_revendas = 0
 
     while numero_de_revendas < 3:
-        x = int(random() * 42)
-        y = int(random() * 42)
-        if not mapa[x][y].valido():
+        x = int(random() * 10)
+        y = int(random() * 10)
+        if not mapa.mapa[x][y].valido():
             continue
         revenda = Revenda(x, y)
         revendas.append(revenda)
@@ -52,31 +24,31 @@ def sortear_pessoas(mapa):
     numero_de_pessoas = 0
 
     while numero_de_pessoas < 3:
-        x = int(random() * 42)
-        y = int(random() * 42)
-        casa_x = int(random() * 42)
-        casa_y = int(random() * 42)
-        if not mapa[x][y].valido():
+        x = int(random() * 10)
+        y = int(random() * 10)
+        casa_x = int(random() * 10)
+        casa_y = int(random() * 10)
+        if not mapa.mapa[x][y].valido():
             continue
-        if not mapa[casa_x][casa_y].valido():
+        if not mapa.mapa[casa_x][casa_y].valido():
             continue
         pessoa = Pessoa(x, y, casa_x, casa_y)
-        pessoas.put([int(random() * 100), pessoa])
+        pessoas.put((random()*100, pessoa))
         numero_de_pessoas += 1
 
     numero_de_pessoas = 0
 
     while numero_de_pessoas < 3:
-        x = int(random() * 42)
-        y = int(random() * 42)
-        casa_x = int(random() * 42)
-        casa_y = int(random() * 42)
-        if not mapa[x][y].valido():
+        x = int(random() * 10)
+        y = int(random() * 10)
+        casa_x = int(random() * 10)
+        casa_y = int(random() * 10)
+        if not mapa.mapa[x][y].valido():
             continue
-        if not mapa[casa_x][casa_y].valido():
+        if not mapa.mapa[casa_x][casa_y].valido():
             continue
         pessoa = Pessoa(x, y, casa_x, casa_y, False)
-        pessoas.put([int(random() * 100), pessoa])
+        pessoas.put((int(random() * 100), pessoa))
         numero_de_pessoas += 1
     return pessoas
 
@@ -84,28 +56,81 @@ def sortear_pessoas(mapa):
 def buscar_alcool_gel(mapa, agente, revendas):
     revenda = choice(revendas)
     print("O Agente está no (%s,%s) e está indo buscar alcool gel na localização (%s,%s)"%(agente.x, agente.y, revenda.x, revenda.y))
-    busca_heuristica(mapa, agente.x, agente.y, revenda.x, revenda.y)
+    origem = mapa.mapa[agente.x][agente.y]
+    destino = mapa.mapa[revenda.x][revenda.y]
+    busca_heuristica(mapa, origem, destino)
     agente.x = revenda.x
     agente.y = revenda.y
     print("O Agente está na localização(%d,%d) pegando alcool gel" % (agente.x, agente.y))
+    input("-------------------------- Pressione Enter para a próxima ação ----------------------------------")
 
 
 def encontrar_pessoa(mapa, agente, pessoa):
     print("O Agente está no (%d,%d) e está indo até na pessoa que está na localização (%d,%d)" % (agente.x, agente.y, pessoa.x, pessoa.y))
-    busca_heuristica(mapa, agente.x, agente.y, pessoa.x, pessoa.y)
+    origem = mapa.mapa[agente.x][agente.y]
+    destino = mapa.mapa[pessoa.x][pessoa.y]
+    busca_heuristica(mapa, origem, destino)
     agente.x = pessoa.x
     agente.y = pessoa.y
     agente.sucesso = pessoa.ajuda
     print("O Agente está na localização(%d,%d) com uma pessoa" % (agente.x, agente.y))
+    input("-------------------------- Pressione Enter para a próxima ação ----------------------------------")
 
 
 def levar_para_casa(mapa, agente, pessoa):
     print("O Agente está no (%d,%d) e levando a pessoa para casa na localização (%d,%d)" % (agente.x, agente.y, pessoa.casa_x, pessoa.casa_y))
-    busca_heuristica(mapa, agente.x, agente.y, pessoa.casa_x, pessoa.casa_y)
+    origem = mapa[agente.x][agente.y]
+    destino = mapa[pessoa.casa_x][pessoa.casa_y]
+    busca_heuristica(mapa, origem, destino)
     agente.x = pessoa.casa_x
     agente.y = pessoa.casa_y
     print("O Agente levou uma pessoa para casa e está na localização(%d,%d)" % (agente.x, agente.y))
+    input("-------------------------- Pressione Enter para a próxima ação ----------------------------------")
 
 
-def busca_heuristica(mapa, origem_x, origem_y, destino_x, destino_y):
+def busca_heuristica(mapa, origem, destino):
+    print("Buscando")
     print("")
+    fronteira = PriorityQueue()
+    fronteira.put([0, origem])
+
+    mapa[origem.x][origem.y].F = 0 + heuristica(origem, destino)
+    mapa[origem.x][origem.y].G = 0
+    while not fronteira.empty():
+        atual = fronteira.get()[1]
+        if atual.x == destino.x and atual.y == destino.y:
+            print("Você chegou no seu destino (%s,%s)"%(atual.x, atual.y))
+            while not fronteira.empty():
+                fronteira.get()
+            break
+
+        for vizinho in vizinhos(atual, mapa):
+            custo_atual = vizinho.peso + atual.G
+
+            if custo_atual < mapa[vizinho.x][vizinho.y].G:
+                mapa[vizinho.x][vizinho.y].G = custo_atual
+                mapa[vizinho.x][vizinho.y].F = custo_atual + heuristica(vizinho, destino)
+                fronteira.put((mapa[vizinho.x][vizinho.y].F, mapa[vizinho.x][vizinho.y]))
+    print("")
+    print("Final da Busca")
+
+
+def vizinhos(atual, mapa):
+    vizinhos = []
+    DIRECOES=[
+        Coordenada(0, -1),
+        Coordenada(0, 1),
+        Coordenada(1, 0),
+        Coordenada(-1, 0)
+    ]
+
+    for direcao in DIRECOES:
+        vizinho = Coordenada(atual.x + direcao.x, atual.y + direcao.y)
+
+        if vizinho.valido() and mapa[vizinho.x][vizinho.y].valido():
+            vizinhos.append(mapa[vizinho.x][vizinho.y])
+    return vizinhos
+
+
+def heuristica(atual, destino):
+    return abs(atual.x - destino.x) + abs(atual.y - destino.y)
--- models.py
+++ models.py
@@ -1,12 +1,88 @@
+import math
+from main import TAMANHO
+
+
+class Mapa():
+    def __init__(self, matriz):
+        x, y = TAMANHO
+        self.mapa = []
+        for i in range(x):
+            self.mapa.append([])
+            coordenada = Coordenada(0, 0)
+            for j in range(y):
+                if matriz[i][j] == "G":
+                    coordenada = Coordenada(i, j, 'G', 10)
+                elif matriz[i][j] == "A":
+                    coordenada = Coordenada(i, j, 'A', 1)
+                elif matriz[i][j] == "P":
+                    coordenada = Coordenada(i, j, 'P', 3)
+                elif matriz[i][j] == "T":
+                    coordenada = Coordenada(i, j, 'T', 6)
+                elif matriz[i][j] == "E":
+                    coordenada = Coordenada(i, j, 'E', -1)
+                self.mapa[i].append(coordenada)
+
+    def reiniciar_mapa(self):
+        x, y = TAMANHO
+        for i in range(x):
+            for j in range(y):
+                self.mapa[i][j].F = math.inf
+                self.mapa[i][j].G = math.inf
+
+    def mostrar_mapa(self):
+        x, y = TAMANHO
+        for i in range(x):
+            for j in range(y):
+                print(self.mapa[i][j], end=" ")
+            print('')
+        print("")
+
+    def mostrar_parede(self):
+        x, y = TAMANHO
+        for i in range(x):
+            for j in range(y):
+                if self.mapa[i][j].peso > 0:
+                    print(".", end=" ")
+                else:
+                    print("#", end=" ")
+            print('')
+        print("")
+
+    def mostrar_mapa_G(self):
+        x, y = TAMANHO
+        for i in range(x):
+            for j in range(y):
+                print(self.mapa[i][j].G, end=" ")
+            print('')
+        print("")
+
+    def mostrar_mapa_F(self):
+        x, y = TAMANHO
+        for i in range(x):
+            for j in range(y):
+                print(self.mapa[i][j].F, end=" ")
+            print('')
+        print("")
+
+
 class Coordenada():
-    def __init__(self, tipo, x, y, peso):
+    def __init__(self, x, y, tipo="*", peso=0):
         self.x = x
         self.y = y
         self.tipo = tipo
         self.peso = peso
+        self.F = math.inf
+        self.G = math.inf
 
     def __str__(self):
         return self.tipo
+
+    def __lt__(self, other):
+        if self.x < other.x:
+            return True
+        elif self.x > other.x:
+            return False
+        return self.y <= other.y
 
     def valido(self):
         if self.tipo == "E":
@@ -22,17 +98,15 @@ class Pessoa():
         self.casa_y = casa_y
         self.ajuda = ajuda
 
+
 class Agente():
     def __init__(self, x, y, sucesso=False):
         self.x = x
         self.y = y
         self.sucesso = sucesso
 
+
 class Revenda():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
-    def __str__(self):
-        return self.nome
-
