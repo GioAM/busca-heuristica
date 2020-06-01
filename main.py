fcabdbdae33d5f500f4c6163f2e89a4de533c2e8
Giovani Meneguel <34317514+GioAM@users.noreply.github.com>
2020-06-01 20:00:19 -0300

    legendas

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from functions import *
 from matriz import matriz
 from models import Mapa
 
-print(" .  Terreno que pode ser usado")
-print(" #  Terreno que pode não ser usado (Edifício)")
-print(" R  Revenda de Alcool Gel")
-print(" P  Pessoa na rua")
-print(" A  Agente")
-print(" C  Casa de uma pessoa")
-print(" |  Agente Passou")
+mapa = Mapa(matriz(), True)
+if mapa.exibir_cores:
+    print('\33[1:40:40m' + "Terreno que pode ser usado" + '\33[0:0m')
+    print('\33[1:30:41m' + "Terreno que pode não ser usado (Edifício)" + '\33[0:0m')
+    print('\33[1:30:45m' + "Revenda de Alcool Gel" + '\33[0:0m')
+    print('\33[1:30:43m' + "Pessoa na rua" + '\33[0:0m')
+    print('\33[1:30:44m' + "Agente" + '\33[0:0m')
+    print('\33[1:30:46m' + "Casa de uma pessoa" + '\33[0:0m')
+    print('\33[1:30:42m' + "Agente Passou" + '\33[0:0m')
+else:
+    print(" .  Terreno que pode ser usado")
+    print(" #  Terreno que pode não ser usado (Edifício)")
+    print(" R  Revenda de Alcool Gel")
+    print(" P  Pessoa na rua")
+    print(" A  Agente")
+    print(" C  Casa de uma pessoa")
+    print(" |  Agente Passou")
 
-mapa = Mapa(matriz())
 pessoas_ajudadas = 0
 pessoas = sortear_pessoas(mapa)
 revendas = sortear_revendas(mapa)
